630e734b97906cd5d88f81cc64ce35f43c843b2d
Holly Hull <hhull@macalester.edu>
2018-10-16 16:53:31 -0500

    make slider stay at current brush size

--- src/pages/color-popover/color-popover.ts
+++ src/pages/color-popover/color-popover.ts
@@ -2,15 +2,20 @@ import { ViewController } from 'ionic-angular';
 import { Component } from '@angular/core';
 import { BrushProvider } from '../../providers/brush/brush'
 
+/*
+  Class for the color-popover menu.
+
+  Menu on DrawingPage that allows user to change brush options, using BrushProvider
+*/
 @Component({
   template: `
     <ion-list>
-      <ion-list-header>Ionic</ion-list-header>
+      <ion-list-header>Brush Options</ion-list-header>
       <button *ngFor="let color of this.brushService.availableColors" icon-only ion-button color="light" (click)="this.brushService.changeColor(color)">
           <ion-icon [style.color]="color" name="brush"></ion-icon>
       </button>
       <ion-item>
-        <ion-range min="1" max="100" color="primary" (ionChange)="this.brushService.changeSize($event)">
+        <ion-range min="1" max="100" color="primary" [(ngModel)]="this.brushService.size" (ionChange)="this.brushService.changeSize($event)">
           <ion-icon range-left style="font-size: 1em;" name="radio-button-on"></ion-icon>
           <ion-icon range-right style="font-size: 2em;" name="radio-button-on"></ion-icon>
         </ion-range>
