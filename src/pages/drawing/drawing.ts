a96ab59df6c38b3f7d519e0111ac8189ba0f1827
Holly Hull <hhull@macalester.edu>
2018-10-17 09:17:01 -0500

    implement overlap at top 2nd, 3rd drawings; fix to only have 3 drawings

--- src/pages/drawing/drawing.ts
+++ src/pages/drawing/drawing.ts
@@ -6,7 +6,6 @@ import { PopoverPage } from '../color-popover/color-popover'
 import { FinalPage } from '../final/final'
 import { BrushProvider } from '../../providers/brush/brush'
 
-
 /**
  * Class for the DrawingPage page.
  *
@@ -21,15 +20,22 @@ import { BrushProvider } from '../../providers/brush/brush'
 
 export class DrawingPage {
   @ViewChild('myCanvas') canvas: ElementRef;
+  @ViewChild('overlap') overlapCanvas: ElementRef;
   @ViewChild(Content) content: Content;
   @ViewChild('headerMenu') header: ElementRef;
 
   canvasElement: any;
+  overlapElement: any;
   lastX: number;
   lastY: number;
 
+  canvasHeight: number;
+  canvasWidth: number;
+
   storedImages = [];
   numCanvases = 0;
+
+  overlapHeight = 20;
 
   constructor(public navCtrl: NavController, public popoverCtrl: PopoverController, public navParams: NavParams, public platform: Platform, public renderer: Renderer, public brushService: BrushProvider) {
   }
@@ -38,41 +44,58 @@ export class DrawingPage {
     this.navCtrl.setRoot(HomePage);
   }
 
-  goToFinalPage(): void {
-    //get the current canvas as an image
+  /*
+   * saves canvas, resets drawing page, sets overlap, and goes to final page if 3 drawings done
+   */
+  nextStep(): void {
+    //clear overlap
+    let ctx = this.overlapElement.getContext('2d');
+    this.clearCanvas(this.overlapElement);
+
+    //get the current canvas as an image, draw it on overlap when loaded
     var img = new Image;
+    img.onload = function(){
+      ctx.drawImage(img, 0, img.height - 20, img.width, 20, 0, 0, ctx.canvas.clientWidth, ctx.canvas.clientHeight); //img.width, img.height, 0, 0, ctx.canvas.clientWidth, ctx.canvas.clientHeight);
+    }
     img.src = this.canvasElement.toDataURL();
 
     //store image in storedImages
     this.storedImages[this.numCanvases] = img;
     this.numCanvases = this.numCanvases + 1;
 
-    this.clearCanvas();
-
-    if(this.numCanvases == 3){
-      this.goToFinalPage();
-    }
+    this.clearCanvas(this.canvasElement);
 
     //if we have 3 pictures, we're done --> go to final page, passing in the Images
-    if(this.numCanvases == 4){
+    if(this.numCanvases == 3){
       this.navCtrl.push(FinalPage, {data: this.storedImages}, {animate:false});
     }
-
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad DrawingPage');
   }
 
+  /*
+   * sets the size of the main canvas and overlap canvas
+   */
   ngAfterViewInit(){
       this.canvasElement = this.canvas.nativeElement;
+      this.overlapElement = this.overlapCanvas.nativeElement;
 
-      var offsetHeight = this.header.nativeElement.offsetHeight + 10; //so it doesn't scroll, subtract header height plus a little more
+      var offsetHeight = this.header.nativeElement.offsetHeight + this.overlapHeight + 5; //so it doesn't scroll, subtract header height plus a little more
+      this.canvasHeight = this.platform.height() - offsetHeight;
+      this.canvasWidth = this.platform.width() - 5;
 
-      this.renderer.setElementAttribute(this.canvasElement, 'width', this.platform.width() + '');
-      this.renderer.setElementAttribute(this.canvasElement, 'height', this.platform.height() - offsetHeight+ '');
+      this.renderer.setElementAttribute(this.overlapElement, 'width', this.canvasWidth + '');
+      this.renderer.setElementAttribute(this.overlapElement, 'height', this.overlapHeight + '');
+
+      this.renderer.setElementAttribute(this.canvasElement, 'width', this.canvasWidth + '');
+      this.renderer.setElementAttribute(this.canvasElement, 'height', this.canvasHeight + '');
   }
 
+  /*
+   * on first tap, make dot @ location and save x, y
+   */
   handleStart(ev){
       var canvasPosition = this.canvasElement.getBoundingClientRect();
 
@@ -86,6 +109,9 @@ export class DrawingPage {
       ctx.fill();
   }
 
+  /*
+   * on drag, draw line from last x, y to current x, y
+   */
   handleMove(ev){
       var canvasPosition = this.canvasElement.getBoundingClientRect();
 
@@ -106,9 +132,9 @@ export class DrawingPage {
       this.lastY = currentY;
   }
 
-  clearCanvas(){
-    let ctx = this.canvasElement.getContext('2d');
-    ctx.clearRect(0, 0, this.canvasElement.width, this.canvasElement.height);
+  clearCanvas(canvas){
+    let ctx = canvas.getContext('2d');
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
   }
 
   presentPopover(myEvent) {
