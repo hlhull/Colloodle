43edc1f7cdfdea16bf768eabcdee4cf328bbc5e5
hlhull <31477048+hlhull@users.noreply.github.com>
2018-10-24 09:13:25 -0500

    delete config.ts

--- src/config.ts
+++
@@ -1,8 +0,0 @@
-export const config = {
-    apiKey: "AIzaSyDPaEnTm99FEYYixNOghQDuUIreGT2_DQE",
-    authDomain: "colloodle-2262a.firebaseapp.com",
-    databaseURL: "https://colloodle-2262a.firebaseio.com",
-    projectId: "colloodle-2262a",
-    storageBucket: "colloodle-2262a.appspot.com",
-    messagingSenderId: "543086140749"
-  };
