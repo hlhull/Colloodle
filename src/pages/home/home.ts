233e63096a3e62991d7aeac4f3f50d219db5955c
Holly Hull <hhull@macalester.edu>
2018-11-01 20:27:39 -0500

    pass imageStorage object to DrawingPage

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -3,6 +3,8 @@ import { NavController, Nav } from 'ionic-angular';
 import { DrawingPage } from '../drawing/drawing';
 import { AuthService } from '../../services/auth.service';
 import { LoginPage } from '../login/login';
+import { LocalStorageProvider } from '../../providers/image-storage/local-storage';
+import { NetworkStorageProvider } from '../../providers/image-storage/network-storage';
 
 @Component({
   selector: 'page-home',
@@ -11,15 +13,12 @@ import { LoginPage } from '../login/login';
 export class HomePage {
 
   /*
-    goes to drawing page, passing in either 'true' for local if user clicked pass-Around
-    and 'false' for local storage if user clicked random (need to store on network)
+    goes to drawing page, passing correct storage provider type: 'true' for local
+    if user clicked pass-Around and 'false' for network storage if user clicked random
   */
-  goToDrawingPage(type): void {
-    if(type == 'pass-around'){
-      this.navCtrl.push(DrawingPage, {local: true}, {animate:false});
-    } else {
-      this.navCtrl.push(DrawingPage, {local: false, group: 'group#', section: '2'}, {animate:false});
-    }
+  goToDrawingPage(local){
+    var imageStorage = local ? new LocalStorageProvider() : new NetworkStorageProvider();
+    this.navCtrl.push(DrawingPage, {imageStorage: imageStorage}, {animate:false});
   }
 
   // @ViewChild(Nav) nav: Nav;
