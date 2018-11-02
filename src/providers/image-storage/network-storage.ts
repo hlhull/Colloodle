488c58e386c4b78bbe6c68bd07f640882272b7dc
Holly Hull <hhull@macalester.edu>
2018-11-01 20:26:37 -0500

    create assignGroup to assign user to a group to draw in

--- src/providers/image-storage/network-storage.ts
+++ src/providers/image-storage/network-storage.ts
@@ -2,27 +2,60 @@ import { Injectable } from '@angular/core';
 import firebase from 'firebase';
 
 /*
-  Generated class for the ImageStorageProvider provider.
-
-  See https://angular.io/guide/dependency-injection for more info on providers
-  and Angular DI.
+  Interacts with Firebase for Random mode to save / get pictures
 */
 @Injectable()
 export class NetworkStorageProvider {
   groupNumber: string;
   sectionNumber: number;
   doneUploading: any;
+  databaseRef = firebase.database().ref();
+  nextListRef = this.databaseRef.child("inProgress").child("next");
 
   constructor() {
-  }
-
-  setUp(group, section){
-    this.groupNumber = group;
-    this.sectionNumber = section;
+    this.nextListRef.push("1");
+    //this.assignGroup();
   }
 
   /*
-   * store image with given dataUrl in Firebase at group/sectionNum.png
+    Assigns group # and section # user will be working on by getting first element
+    in the inProgress:next list; if there is none in inProgress:next, work on 0th section
+  */
+  assignGroup(){
+    // if there's no next group, group# and section# will be set to null --> will actually
+    // create group once the user completes their picture, so we
+    // don't create a group, have user exit w/o submitting image, and then have empty group
+    this.databaseRef.child("inProgress").once('value', function(snapshot){
+      if(!snapshot.hasChild("next")){
+        this.groupNumber = null;
+        this.sectionNumber = 0;
+        console.log(this.groupNumber, this.sectionNumber);
+      }
+    }.bind(this));
+
+    //if there is a next group, remove it from next, set group / section number, and move group to pending
+    this.nextListRef.limitToFirst(1).once("child_added", function(snapshot) {
+        this.groupNumber = snapshot.key;
+        this.sectionNumber = snapshot.val();
+        this.nextListRef.child(this.groupNumber).remove();
+        firebase.database().ref().child("inProgress").child("pending").child(this.groupNumber).set(this.sectionNumber);
+      }.bind(this))
+  }
+
+  /*
+    Returns the previous image in the group, or null if this is the first image
+  */
+  getOverlap(){
+    if(this.sectionNumber > 0){
+      var storageRef = firebase.storage().ref().child(this.groupNumber); //which folder we want to get images from
+      var imageRef = storageRef.child(this.sectionNumber - 1 + '.png'); // references previous image
+      return imageRef.getDownloadURL();
+    }
+    return null;
+  }
+
+  /*
+    store image with given dataUrl in Firebase at group/sectionNum.png
    */
   storeImage(imgUrl){
       var blob = this.dataUrlToBlob(imgUrl);
@@ -38,7 +71,7 @@ export class NetworkStorageProvider {
   }
 
   /*
-   * gets the image urls that are in the specificed group folder in firebase
+    gets the image urls that are in the specificed group folder in firebase
    */
   getImageUrls(){
       var storageRef = firebase.storage().ref().child(this.groupNumber); //which folder we want to get images from
@@ -51,8 +84,9 @@ export class NetworkStorageProvider {
       return Promise.all([imageRef0.getDownloadURL(), imageRef1.getDownloadURL(), imageRef2.getDownloadURL()]);
   }
 
-  // convert base64/URLEncoded data component to raw binary data held in a string
-  // from: https://stackoverflow.com/questions/4998908/convert-data-uri-to-file-then-append-to-formdata/5100158#5100158
+  /* convert base64/URLEncoded data component to raw binary data held in a string
+     from: https://stackoverflow.com/questions/4998908/convert-data-uri-to-file-then-append-to-formdata/5100158#5100158
+  */
   dataUrlToBlob(dataUrl){
       var byteString;
       if (dataUrl.split(',')[0].indexOf('base64') >= 0){
