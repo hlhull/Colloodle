a350baca44d628a34bc35ee861bc3b1d043dd941
Holly Hull <hhull@macalester.edu>
2018-11-03 15:39:41 -0500

    finish updateGroup for when 2nd or 3rd drawing completes

--- src/providers/image-storage/network-storage.ts
+++ src/providers/image-storage/network-storage.ts
@@ -16,8 +16,8 @@ export class NetworkStorageProvider {
     this.groupNumber = "group#";     //use these 2 lines for testing, comment out the "assignGroup()" line to make it actually run
     this.sectionNumber = 2;
 
-    //this.assignGroup();
-
+    // this.assignGroup();
+    
   }
 
   /*
@@ -25,6 +25,7 @@ export class NetworkStorageProvider {
     in the inProgress:next list; if there is none in inProgress:next, work on 0th section
   */
   assignGroup(){
+    var self = this;
     this.databaseRef.child("inProgress").once('value', function(snapshot){
       if(!snapshot.hasChild("next")){
         // if there's no next group, group# and section# will be set to null --> will actually
@@ -35,13 +36,11 @@ export class NetworkStorageProvider {
         console.log(this.groupNumber, this.sectionNumber);
       } else {
         //if there is a next group, remove it from next, set group / section number, and move group to pending
-        this.nextListRef.limitToFirst(1).once('value', function(snapshot) {
-            if(this.groupNumber)
+        this.nextListRef.limitToFirst(1).once('child_added', function(snapshot) {
             this.groupNumber = snapshot.key;
             this.sectionNumber = snapshot.val();
             this.nextListRef.child(this.groupNumber).remove();
             firebase.database().ref().child("inProgress").child("pending").child(this.groupNumber).set(this.sectionNumber);
-            console.log(this.groupNumber, this.sectionNumber);
           }.bind(this))
       }
     }.bind(this));
@@ -84,25 +83,26 @@ export class NetworkStorageProvider {
     creates group if this was first drawing in group
   */
   updateGroup(){
+    var promise;
     // if no group has been made yet (this was first drawing) --> push to Firebase,
     // assign groupNumber as the uid of that push
-    var promise;
-    if(this.sectionNumber == 0){
-      var self = this; //self.sectionNumber + 1
+    if(this.sectionNumber == 0) {
+      var self = this;
       promise = this.nextListRef.push(1).then((ref) => self.groupNumber = ref.getKey());
-    } else {
+    }
+    // if this was the 2nd drawing, remove group from pending and put back into next
+    else if (this.sectionNumber == 1) {
+      this.databaseRef.child("inProgress").child("pending").child(this.groupNumber).remove();
+      this.nextListRef.child(this.groupNumber).set(2);
+      promise = new Promise(function(resolve, reject) {resolve(true)});
+    }
+    // if this was the last drawing, move group to completed
+    else if (this.sectionNumber == 2) {
+      this.databaseRef.child("inProgress").child("pending").child(this.groupNumber).remove();
+      this.databaseRef.child("completed").child(this.groupNumber).set(0);
       promise = new Promise(function(resolve, reject){resolve(true)});
     }
 
-
-    // if(this.sectionNumber == 0){ // if this was the first drawing, create group and move to next
-
-    // } else if (this.sectionNumber == 1){ //if this was the 2nd drawing, move back to next queue
-    //   //this.moveGrouptoNext();
-    // } else if (this.sectionNumber == 2){ //if this was the last drawing, group is completed
-    //   //this.moveGrouptoCompleted();
-    // }
-    // return new Promise(function(resolve, reject) {resolve(self.groupNumber)});
     return promise;
   }
 
