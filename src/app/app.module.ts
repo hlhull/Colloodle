f9b7b05ab71a9dedcd579f45c1b8719c95033055
kgurgel <kgurgel@macalester.edu>
2018-11-06 12:45:40 -0600

    added reset password page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { StatusBar } from '@ionic-native/status-bar';
 import { MyApp } from './app.component';
 import { LoginPage } from '../pages/login/login';
 import { SignupPage } from '../pages/signup/signup';
+import { ResetPasswordPage } from '../pages/reset-password/reset-password';
 import { HomePage } from '../pages/home/home';
 import { DrawingPage } from '../pages/drawing/drawing';
 import { FinalPage } from '../pages/final/final';
@@ -36,6 +37,7 @@ import { ScreenOrientation } from '@ionic-native/screen-orientation';
     MyApp,
     LoginPage,
     SignupPage,
+    ResetPasswordPage,
     HomePage,
     DrawingPage,
     FinalPage,
@@ -55,6 +57,7 @@ import { ScreenOrientation } from '@ionic-native/screen-orientation';
     MyApp,
     LoginPage,
     SignupPage,
+    ResetPasswordPage,
     HomePage,
     DrawingPage,
     PopoverPage,
