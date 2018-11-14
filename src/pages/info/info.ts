2e796b45d5ca37b2e8d9d98f7e789296d003df2f
Katia Sievert <ksievert@macalester.edu>
2018-11-14 13:02:49 -0600

    basic info page, still working on adding images

--- src/pages/info/info.ts
+++ src/pages/info/info.ts
@@ -6,6 +6,9 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
  *
  * See https://ionicframework.com/docs/components/#navigation for more info on
  * Ionic pages and navigation.
+ *
+ * code from ionic-preview-app
+ *
  */
 
 @IonicPage()
@@ -14,7 +17,28 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   templateUrl: 'info.html',
 })
 export class InfoPage {
-
+  slides = [
+    {
+      title: "Drawn Together",
+      description: "<b>Make masterpieces with the people around you!</b> Each player draws one section of a zany hybrid creation. The more ridiculous, the better!",
+      image: "assets/img/info-slide-1.png",
+    },
+    {
+      title: "Head, Torso, Body",
+      description: "You'll be assigned to draw either the <b>head</b> (face and neck), <b>body</b> (shirt area), or <b>legs</b> (pants and shoes area) of your drawing.",
+      image: "assets/img/info-slide-2.png",
+    },
+    {
+      title: "Stay Connected",
+      description: "If you're drawing the head or torso, <b>make sure to draw to the bottom of your screen</b>! It helps the next player <b>connect their lines to the previous player's art</b>.",
+      image: "assets/img/info-slide-3.png",
+    },
+    {
+      title: "Two Modes of Play",
+      description: "Hanging out with some friends? Play with <b>Pass Around</b> to take turns drawing on one device. Flying solo? Login with <b>Connect Mode</b> to play with other users!",
+      image: "assets/img/info-slide-4.png",
+    }
+  ];
   constructor(public navCtrl: NavController, public navParams: NavParams) {
   }
 
