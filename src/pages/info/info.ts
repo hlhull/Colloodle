874f3244d99e8679edcdb1a4483d50c94fb87d89
Katia Sievert <ksievert@macalester.edu>
2018-11-14 16:40:22 -0600

    cleaned up info page draft, renamed some buttons

--- src/pages/info/info.ts
+++ src/pages/info/info.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { HomePage} from '../home/home';
 
 /**
  * Generated class for the InfoPage page.
@@ -21,25 +22,29 @@ export class InfoPage {
     {
       title: "Drawn Together",
       description: "<b>Make masterpieces with the people around you!</b> Each player draws one section of a zany hybrid creation. The more ridiculous, the better!",
-      image: "assets/img/info-slide-1.png",
+      image: "assets/imgs/info-slide-1.png",
     },
     {
       title: "Head, Torso, Body",
       description: "You'll be assigned to draw either the <b>head</b> (face and neck), <b>body</b> (shirt area), or <b>legs</b> (pants and shoes area) of your drawing.",
-      image: "assets/img/info-slide-2.png",
+      image: "assets/imgs/info-slide-2.png",
     },
     {
       title: "Stay Connected",
-      description: "If you're drawing the head or torso, <b>make sure to draw to the bottom of your screen</b>! It helps the next player <b>connect their lines to the previous player's art</b>.",
-      image: "assets/img/info-slide-3.png",
+      description: "If you're drawing the head or torso, make sure to <b>draw to the bottom of your screen</b>! It helps the next player <b>connect their lines to the previous player's art</b>.",
+      image: "assets/imgs/info-slide-3.png",
     },
     {
       title: "Two Modes of Play",
       description: "Hanging out with some friends? Play with <b>Pass Around</b> to take turns drawing on one device. Flying solo? Login with <b>Connect Mode</b> to play with other users!",
-      image: "assets/img/info-slide-4.png",
+      image: "assets/imgs/info-slide-4.png",
     }
   ];
   constructor(public navCtrl: NavController, public navParams: NavParams) {
+  }
+
+  goHome(): void {
+    this.navCtrl.setRoot(HomePage);
   }
 
   ionViewDidLoad() {
