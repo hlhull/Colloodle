7226888d2a0a409993a4560214b33a4e00aae378
kgurgel <kgurgel@macalester.edu>
2018-11-28 22:27:22 -0600

    made the brush popover menu's header a little nicer

--- src/pages/color-popover/color-popover.ts
+++ src/pages/color-popover/color-popover.ts
@@ -8,14 +8,14 @@ import { BrushProvider } from '../../providers/brush/brush'
   Menu on DrawingPage that allows user to change brush options, using BrushProvider
 */
 @Component({
-  template: `
+  template: ` 
     <ion-list>
-      <ion-list-header>
-        <h2 ion-text [style.color]="this.brushService.styleColor">Brush Options</h2>
-        <button ion-button icon-only (click)="this.close()" style="float: right;" color="dark" clear>
+      <ion-item>
+        <h2 ion-text [style.color]="dark">Brush Options</h2>
+        <button ion-button icon-only (click)="this.close()" item-right color="dark" clear>
           <ion-icon name="close" align="right"></ion-icon>
         </button>
-      </ion-list-header>
+      </ion-item>
       <button *ngFor="let color of this.brushService.availableColors" icon-only ion-button clear (click)="this.brushService.changeColor(color)">
           <ion-icon [style.color]="color" name="square"></ion-icon>
       </button>
